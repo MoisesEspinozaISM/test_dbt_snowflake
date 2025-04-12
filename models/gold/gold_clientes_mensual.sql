e8f0a5ef905f10fc079be03575443b524c829ca3
MoisesEspinozaISM <moises.espinoza@ism.global>
2025-04-12 15:47:51 -0500

    update gold test

---
+++ models/gold/gold_clientes_mensual.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='table',
+    schema='public',
+    alias='gold_clientes_mensual'
+) }}
+
+SELECT
+    customer_id,
+    customer_name,
+    DATE_TRUNC('month', order_date) AS mes,
+    COUNT(order_id) AS total_pedidos,
+    ROUND(SUM(total_price), 2) AS total_ventas,
+    ROUND(AVG(total_price), 2) AS ticket_promedio
+FROM {{ ref('silver_orders_enriched') }}
+GROUP BY 1, 2, 3
+ORDER BY 1, 3
